e7897d905a80c0a240db203fb426ec91af70febb
andip71 <andreasp@gmx.de>
2014-05-31 18:33:44 +0200

    Boeffla ramfs: rebase bccontroller.sh and boeffla-init.sh

--- ramdisk_boeffla/res/bc/bccontroller.sh
+++ ramdisk_boeffla/res/bc/bccontroller.sh
@@ -12,7 +12,8 @@
 KERNEL_SPECS="i9300;samsung;jb44;http://boeffla.df-kunde.de/sgs3/boeffla-kernel/"
 
 # kernel features 
-# (1=enable-busybox,2=enable-frandom,3=wipe-cache)
+# (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
+# (5=enable-default-zram-control)
 KERNEL_FEATURES="-1-2-3-"
 
 # path to kernel libraries
