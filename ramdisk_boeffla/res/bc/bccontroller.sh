d0951bdf3de9c867d0aa9eb8334c521d5376578c
andip71 <andreasp@gmx.de>
2014-09-01 07:45:26 +0200

    Boeffla ramfs: Good bye unified bccontroller script

--- ramdisk_boeffla/res/bc/bccontroller.sh
+++ ramdisk_boeffla/res/bc/bccontroller.sh
@@ -1,8 +1,11 @@
 # Boeffla-Config controller interface
 #
-# Version: GPU 5 frequencies
+# *****************************
+# i9300 Samsung 4.3 version
 #
-# (C) andip71
+# V0.1
+# *****************************
+
 
 # ********************************
 # Kernel specific initialisation
@@ -17,8 +20,7 @@ KERNEL_SPECS="i9300;samsung;jb43;http://boeffla.df-kunde.de/sgs3/boeffla-kernel/
 KERNEL_FEATURES="-1-2-3-"
 
 # path to kernel libraries
-LIBPATH="/lib/modules"				# Samsung
-#LIBPATH="/system/lib/modules"		# Cyanogenmod+Omni
+LIBPATH="/lib/modules"
 
 # block devices
 SYSTEM_DEVICE="/dev/block/mmcblk0p9"
