acf5fe03bde4d24ecdd2b0ceaa70cdae8bef45de
andip71 <andreasp@gmx.de>
2014-11-19 11:52:00 +0100

    Boeffla ramfs: Hardening for all reboot activities by disabling dynamic fsync before reboot

--- ramdisk_boeffla/res/bc/bccontroller.sh
+++ ramdisk_boeffla/res/bc/bccontroller.sh
@@ -1346,6 +1346,7 @@ if [ "action_debug_info_file" == "$1" ]; then
 fi
 
 if [ "action_reboot" == "$1" ]; then
+	echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
 	busybox sync
 	busybox sleep 1s
 	/system/bin/reboot
@@ -1353,6 +1354,7 @@ if [ "action_reboot" == "$1" ]; then
 fi
 
 if [ "action_reboot_cwm" == "$1" ]; then
+	echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
 	busybox sync
 	busybox sleep 1s
 	/system/bin/reboot recovery
@@ -1360,6 +1362,7 @@ if [ "action_reboot_cwm" == "$1" ]; then
 fi
 
 if [ "action_reboot_download" == "$1" ]; then
+	echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
 	busybox sync
 	busybox sleep 1s
 	/system/bin/reboot download
@@ -1367,6 +1370,7 @@ if [ "action_reboot_download" == "$1" ]; then
 fi
 
 if [ "action_wipe_caches_reboot" == "$1" ]; then
+	echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
 	busybox rm -rf /cache/*
 	busybox rm -rf /data/dalvik-cache/*
 	busybox sync
